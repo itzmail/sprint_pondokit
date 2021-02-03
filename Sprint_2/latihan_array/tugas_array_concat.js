4ede8ab0e09c7b4a1ed8163cd5c0d74b71362baf
ismailify <ismailnuralam@gmail.com>
2021-02-04 04:08:04 +0700

    Tugas Hari Rabu tanggal 3 Februari

---
+++ Sprint_2/latihan_array/tugas_array_concat.js
@@ -0,0 +1,5 @@
+var reactNative = ['Hafif , Dayat, Wandi']
+var reactJs = ['Yusuf, Luthfi, Tompel']
+var JavaScriptDivision = reactJs.concat(reactNative)
+
+console.log(JavaScriptDivision)
---
+++ Sprint_2/latihan_array/tugas_array_mean.js
@@ -0,0 +1,8 @@
+var nilaiSantri = [ 5, 9, 6, 7, 9, 8, 10, 7, 8 ];
+var total = 0
+
+for(var i=0; i<nilaiSantri.length; i++){
+    total += nilaiSantri[i];
+}
+
+var mean = console.log(total/nilaiSantri.length)
