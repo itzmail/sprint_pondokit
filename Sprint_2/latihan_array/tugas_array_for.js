e2452052caed95b3bb9bb479eb52ee1a7eddb59c
ismailify <ismailnuralam@gmail.com>
2021-02-04 04:13:41 +0700

    memebetulkan fungso for

--- Sprint_2/latihan_array/tugas_array_for.js
+++ Sprint_2/latihan_array/tugas_array_for.js
@@ -1,5 +1,5 @@
 var santriPondok = ['Wandi','Aldi','Acep','Putra','Hafif','Syofyan',"Rahmad"]
 
 for(var i=0; i<santriPondok.length; i++){
-    console.log((i+1) + '.' + santriPondok)
-}
+    console.log((i+1) + '.' + santriPondok[i])
+}cd
